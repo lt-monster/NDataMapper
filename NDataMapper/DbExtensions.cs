5fcb6e534c210e94d406de98ce83fc069537acb1
17740960446 <1023785565@qq.com>
2022-04-28 23:24:52 +0800

    1.添加全局using； 2.缓存泛型类型； 3.对象转常见类型； 4.新增QueryFirst方法；

--- NDataMapper/DbExtensions.cs
+++ NDataMapper/DbExtensions.cs
@@ -1,12 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace NDataMapper;
 
-namespace NDataMapper
+public static class DbExtensions
 {
-    internal class DbExtensions
+    public static T? QueryFirst<T>(this IDbConnection conn, string sql, object? para = null)
     {
+        if (string.IsNullOrWhiteSpace(sql)) return default;
+        if(conn.State == ConnectionState.Closed) conn.Open();
+        using var cmd = conn.CreateCommand();
+        cmd.CommandText = sql;
+        var targetType = typeof(T);
+        if (targetType.IsValueType || targetType == typeof(string) || targetType.IsArray)
+        {
+            return MapperUtils.GetValue<T>(cmd.ExecuteScalar());
+        }
+        object? targetValue = null;
+        using var reader = cmd.ExecuteReader(CommandBehavior.SingleRow);
+        if (reader.Read())
+        {
+            targetValue = Activator.CreateInstance(targetType);
+            for (int i = 0; i < reader.FieldCount; i++)
+            {
+                if (MapperUtils.GetPropertyInfo(targetType, reader.GetName(i), out var p))
+                {
+                    p?.SetValue(targetValue, reader.GetValue(i));
+                }
+            }
+        }
+        return (T?)targetValue;
     }
 }
---
+++ NDataMapper/MapperUtils.cs
@@ -0,0 +1,77 @@
+﻿namespace NDataMapper;
+
+internal class MapperUtils
+{
+    private static readonly Dictionary<Type, Dictionary<string, PropertyInfo>> psCaches = new();
+
+    internal static bool GetCachePropertis(Type type, out Dictionary<string, PropertyInfo>? ps) => psCaches.TryGetValue(type, out ps);
+
+    internal static bool GetPropertyInfo(Type type, string name, out PropertyInfo? property)
+    {
+        if (!psCaches.ContainsKey(type))
+        {
+            psCaches.Add(type, new());
+            type.GetProperties().ToList().ForEach(p =>
+            {
+                string ncolumnName = p.Name.ToLower();
+                if (p.GetCustomAttribute<NColumnAttribute>() is NColumnAttribute nColumn)
+                {
+                    if (!string.IsNullOrWhiteSpace(nColumn.ColumnName)) ncolumnName = nColumn.ColumnName;
+                }
+                else if (p.GetCustomAttribute<ColumnAttribute>() is ColumnAttribute dColumn)
+                {
+                    if (!string.IsNullOrWhiteSpace(dColumn.Name)) ncolumnName = dColumn.Name;
+                }
+                psCaches[type].TryAdd(ncolumnName, p);
+            });
+        }
+        return psCaches[type].TryGetValue(name, out property);
+    }
+
+    internal static int GetTypePropertyCount(Type type)
+        => psCaches.TryGetValue(type, out var ps) ? ps.Count : 0;
+
+    internal static T? GetValue<T>(object? obj)
+    {
+        try
+        {
+            var targetType = typeof(T);
+            if (obj is null || Convert.IsDBNull(obj)
+                || (targetType != typeof(string) && !targetType.IsValueType)) return default;
+            if (obj is T tval) return tval;
+            if (obj is Array array && targetType.IsArray)
+            {
+                var elementType = targetType.GetElementType();
+                elementType = Nullable.GetUnderlyingType(elementType!) ?? elementType;
+                var result = Array.CreateInstance(elementType!, array.Length);
+                for (int i = 0; i < array.Length; i++)
+                    result.SetValue(Convert.ChangeType(array.GetValue(i), elementType!), i);
+                return (T)(object)result;
+            }
+            if (obj is DateTime date && (targetType == typeof(DateOnly) || targetType == typeof(DateOnly?))) 
+                return (T)(object)DateOnly.FromDateTime(date);
+            if (obj is DateTime time && (targetType == typeof(TimeOnly) || targetType == typeof(TimeOnly?)))
+                return (T)(object)TimeOnly.FromDateTime(time);
+            targetType = Nullable.GetUnderlyingType(targetType) ?? targetType;
+            return (T)Convert.ChangeType(obj, targetType);
+        }
+        catch
+        {
+            return default;
+        }
+    }
+
+    /// <summary>
+    /// 是否是匿名类型
+    /// </summary>
+    /// <param name="type"></param>
+    /// <returns></returns>
+    internal static bool IsAnonymousType(Type type) => type.FullName?.StartsWith("<>f__AnonymousType") is true;
+}
+
+[AttributeUsage(AttributeTargets.Property)]
+public class NColumnAttribute: Attribute
+{
+    public string? ColumnName { get; set; }
+    public NColumnAttribute(string columnName) => ColumnName = columnName;
+}
